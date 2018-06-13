c1d7dbb9a1c1f424a66dfc52193bd5c28957de9e
José Blanco (attakinsky) <jose.blanco@grupoperinola.com>
2018-06-13 09:26:04 -0600

    Solución al problema dle home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,60 +38,58 @@ $articulos = [
         </div>
         <div class="row">
           <div class="col-lg-6 col-md-12">
-              <h1>Pregunta Del Mes</h1>
-            <div class="card flex-md-row mb-4 box-shadow h-md-250">
-              <div class="card-body d-flex flex-column align-items-start">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top img-fluid" src="{{ asset('lfep/imgs/pregunta.jpg') }}" data-holder-rendered="true">
+              <div class="card-body">
 
                 <h3 class="answerTitle">
-                  <a class="text-dark" href="{{ route('pregunta', $question->slug) }}">{{ ucfirst($question->created_at->format('F')) }}</a>
+                  <a class="text-dark" href="{{ route('pregunta', $question->slug) }}">Pregunta Del Mes</a>
                 </h3>
                 <p class="card-text mb-auto"><strong>{{ $question->title }}</strong><br>{!! str_limit($question->answer, 130) !!}</p>
                 <a href="{{ route('pregunta', $question->slug) }}" class="answerLink">Leer la respuesta</a>
               </div>
-              <img class="card-img-right flex-auto d-none d-md-block" alt="Thumbnail [200x250]" style="width: auto; height: 200px;" src="{{ asset('lfep/imgs/pregunta.jpg') }}" data-holder-rendered="true">
               
+              <div class="card-footer text-right">
+                  <a href="{{ route('preguntas') }}">Leer todas las preguntas</a>
+                </div>
             </div>
-            <div class="col-md-12 text-right">
-                <a href="{{ route('preguntas') }}">Leer todas</a>
-              </div>
           </div>
 
           <div class="col-lg-6 col-md-12">
-              <h1>Videos</h1>
-            <div class="card flex-md-row mb-4 box-shadow h-md-250">
-              <div class="card-body d-flex flex-column align-items-start">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top img-fluid"  src="{{ asset('lfep/imgs/consejero.jpg') }}" data-holder-rendered="true">
+              <div class="card-body">
                 <h3 class="answerTitle">
                   <a class="text-dark" href="{{ route('video') }}">Conferencias en video</a>
                 </h3>
                 <p class="card-text mb-auto">Extractos de las conferencias "Consejos para fortalecer su matrimonio" y "Excelencia en la Familia".</p>
                 <a href="{{ route('video') }}" class="answerLink">Ver vídeos</a>
               </div>
-              <img class="card-img-right flex-auto d-none d-md-block" alt="Thumbnail [200x250]" style="width: auto; height: 200px;" src="{{ asset('lfep/imgs/consejero.jpg') }}" data-holder-rendered="true">
             </div>
           </div>
           <div class="col-lg-6 col-md-12">
                 
-            <div class="card flex-md-row mb-4 box-shadow h-md-250">
-              <div class="card-body d-flex flex-column align-items-start">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="{{ asset('images/fotos/09.jpg') }}" data-holder-rendered="true">
+              <div class="card-body">
                 <h3 class="answerTitle">
                   <a class="text-dark" href="{{ route('fotos') }}">Fotos</a>
                 </h3>
                 <p class="card-text mb-auto">Galería de imágenes del Pastor Víctor Súchite.</p>
                 <a href="{{ route('fotos') }}" class="answerLink">Ver fotos</a>
               </div>
-              <img class="card-img-right flex-auto d-none d-md-block" alt="Thumbnail [200x250]" style="width: auto; height: 250px;" src="{{ asset('images/fotos/09.jpg') }}" data-holder-rendered="true">
             </div>
           </div>
           <div class="col-lg-6 col-md-12">
-            <div class="card flex-md-row mb-4 box-shadow h-md-250">
-              <div class="card-body d-flex flex-column align-items-start">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="{{ asset('lfep/imgs/edyn2.jpg') }}" data-holder-rendered="true">
+              <div class="card-body">
                 <h3 class="answerTitle">
                   <a class="text-dark" href="{{ route('descargar', 'el-credo-del-matrimonio-y-la-familia.pdf') }}" target="_blank">PDF gratuito</a>
                 </h3>
                 <p class="card-text mb-auto">El credo del matrimonio y la familia.</p>
                 <a href="{{ route('descargar', 'el-credo-del-matrimonio-y-la-familia.pdf') }}" target="_blank" class="answerLink">Descargar</a>
               </div>
-              <img class="card-img-right flex-auto d-none d-md-block" alt="Thumbnail [200x250]" style="width: auto; height: 250px;" src="{{ asset('lfep/imgs/edyn2.jpg') }}" data-holder-rendered="true">
             </div>
           </div>
         </div>
